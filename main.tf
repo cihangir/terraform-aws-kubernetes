1c5bf31c29d1ead7f750152a15d8cca4b4847409
Cihangir SAVAS <cihangir@koding.com>
2016-04-08 01:15:41 -0700

    main: run hclfmt

--- main.tf
+++ main.tf
@@ -1,36 +1,44 @@
-variable "access_key" { }
+variable "access_key" {}
 
-variable "secret_key" { }
+variable "secret_key" {}
 
-variable "name" { }
+variable "name" {}
 
-variable "instance_type_master" { default = "m3.xlarge" }
-variable "instance_type_node"   { default = "m3.xlarge" }
-variable "instance_type_etcd"   { default = "m3.xlarge" }
+variable "instance_type_master" {
+  default = "m3.xlarge"
+}
+
+variable "instance_type_node" {
+  default = "m3.xlarge"
+}
+
+variable "instance_type_etcd" {
+  default = "m3.xlarge"
+}
 
 module "kube_master" {
-    source        = "github.com/cihangir/terraform-aws"
-    instance_type = "${var.instance_type_master}"
-    access_key    = "${var.access_key}"
-    secret_key    = "${var.secret_key}"
-    name          = "${var.name}-master"
-    ami_id        = "ami-74dcfc17"
+  source        = "github.com/cihangir/terraform-aws"
+  instance_type = "${var.instance_type_master}"
+  access_key    = "${var.access_key}"
+  secret_key    = "${var.secret_key}"
+  name          = "${var.name}-master"
+  ami_id        = "ami-74dcfc17"
 }
 
 module "kube_nodes" {
-    source        = "github.com/cihangir/terraform-aws"
-    instance_type = "${var.instance_type_node}"
-    access_key    = "${var.access_key}"
-    secret_key    = "${var.secret_key}"
-    name          = "${var.name}-node"
-    ami_id        = "ami-74dcfc17"
+  source        = "github.com/cihangir/terraform-aws"
+  instance_type = "${var.instance_type_node}"
+  access_key    = "${var.access_key}"
+  secret_key    = "${var.secret_key}"
+  name          = "${var.name}-node"
+  ami_id        = "ami-74dcfc17"
 }
 
 module "kube_etcd" {
-    source        = "github.com/cihangir/terraform-aws"
-    instance_type = "${var.instance_type_etcd}"
-    access_key    = "${var.access_key}"
-    secret_key    = "${var.secret_key}"
-    name          = "${var.name}-etcd"
-    ami_id        = "ami-74dcfc17"
+  source        = "github.com/cihangir/terraform-aws"
+  instance_type = "${var.instance_type_etcd}"
+  access_key    = "${var.access_key}"
+  secret_key    = "${var.secret_key}"
+  name          = "${var.name}-etcd"
+  ami_id        = "ami-74dcfc17"
 }
