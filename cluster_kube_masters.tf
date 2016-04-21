e398ed73e64d96d0ce923fb47c59692dedfccbcf
Cihangir SAVAS <cihangir@koding.com>
2016-04-21 01:24:12 -0700

    cluster: use node template for kube nodes

--- cluster_kube_masters.tf
+++ cluster_kube_masters.tf
@@ -6,6 +6,7 @@ module "aws_elb_kube_masters" {
   name                        = "${var.name}-masters"
   aws_vpc_id                  = "${module.aws_vpc.aws_vpc_vpc_id}"
   aws_subnet_subnet_ids       = "${module.aws_vpc.aws_subnet_subnet_ids}"
+
   aws_elb_instance_port       = 8080
   aws_elb_instance_protocol   = "tcp"
   aws_elb_port                = 8080
@@ -20,7 +21,7 @@ module "aws_asg_kube_masters" {
   key_name              = "${module.aws_vpc.aws_key_name}"
   load_balancer_names   = "${module.aws_elb_kube_masters.aws_elb_elb_name}"
   instance_type         = "${var.instance_type_master}"
-  ami_id                = "ami-5fb63833"
+  ami_id                = "ami-d77cf2bb"
   desired_cluster_size  = "${var.master_desired_cluster_size}"
 
   rendered_cloud_init = "${template_file.kube_master_cloud_init_file.rendered}"
--- cluster_kube_nodes.tf
+++ cluster_kube_nodes.tf
@@ -6,6 +6,12 @@ module "aws_elb_kube_nodes" {
   name                  = "${var.name}-nodes"
   aws_vpc_id            = "${module.aws_vpc.aws_vpc_vpc_id}"
   aws_subnet_subnet_ids = "${module.aws_vpc.aws_subnet_subnet_ids}"
+
+  aws_elb_instance_port       = 8080
+  aws_elb_instance_protocol   = "tcp"
+  aws_elb_port                = 8080
+  aws_elb_protocol            = "tcp"
+  aws_elb_health_check_target = "HTTP:8080/healthz"
 }
 
 
@@ -16,10 +22,10 @@ module "aws_asg_kube_nodes" {
   key_name              = "${module.aws_vpc.aws_key_name}"
   load_balancer_names   = "${module.aws_elb_kube_nodes.aws_elb_elb_name}"
   instance_type         = "${var.instance_type_master}"
-  ami_id                = "ami-5fb63833"
+  ami_id                = "ami-d77cf2bb"
   desired_cluster_size  = "${var.master_desired_cluster_size}"
 
-  rendered_cloud_init = "${template_file.kube_master_cloud_init_file.rendered}"
+  rendered_cloud_init = "${template_file.kube_node_cloud_init_file.rendered}"
   security_groups     = "${module.aws_elb_kube_nodes.aws_elb_elb_aws_security_group_sec_group_id},${module.aws_sg.aws_security_group_sec_group_id}"
 }
 
