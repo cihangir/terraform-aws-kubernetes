831f650c47cbd44ff4dd9b78f85ccbde01fa8795
Cihangir SAVAS <cihangir@koding.com>
2016-04-22 03:00:41 -0700

    cluster: remove unnecessary security rules

--- cluster_kube_nodes.tf
+++ cluster_kube_nodes.tf
@@ -28,13 +28,3 @@ module "aws_asg_kube_nodes" {
   rendered_cloud_init = "${template_file.kube_node_cloud_init_file.rendered}"
   security_groups     = "${module.aws_elb_kube_nodes.aws_elb_elb_aws_security_group_sec_group_id},${module.aws_sg.aws_security_group_sec_group_id}"
 }
-
-# Allow all incoming communication to ETCD elb from kube nodes
-resource "aws_security_group_rule" "allow_all_ingress_from_kube_nodes_to_etcd_elb" {
-  type                     = "ingress"
-  from_port                = 0
-  to_port                  = 0
-  protocol                 = "-1"
-  source_security_group_id = "${module.aws_elb_kube_nodes.aws_elb_elb_aws_security_group_sec_group_id}"
-  security_group_id        = "${module.aws_elb_etcd.aws_elb_elb_aws_security_group_sec_group_id}"
-}
